98fdf806b8ce71069f8b1d5de08bb72ae28f8caa
Varun_Parnandi <parnandivarun@gmail.com>
2026-01-11 12:12:06 +0530

    Delete front-end/personal-loan-app directory
    duplicate directory found and deleted

--- front-end/personal-loan-app/src/services/paymentService.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/api/payments" });
-
-export const makePayment = (loanId, data) => API.post(`/${loanId}`, data);
-export const getPayments = (loanId) => API.get(`/${loanId}`);
